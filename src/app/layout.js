700b8d5365fdbce061b3cbe8bbe9989b9869e053
Rifqi Aquila <andifarifqi67@gmail.com>
2026-01-28 22:45:39 +0700

    ganti logo tab

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portofolio Andifa Rifqi",
   description: "Full-Stack Developer Portfolio",
+   icons: {
+    icon: [
+      { url: "/icon.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
